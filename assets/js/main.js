4efab86b745f3bb6b61137bcb7fb2f64cf47f4cf
Ihor Orlovskyi <ihor.droid@gmail.com>
2015-10-22 05:17:43 +0300

    add angular and firebase

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,25 @@
 * https://github.com/ChrisWojcik/single-page-nav
 */
 $('#anhors').singlePageNav({
-	easing: 'easeInOutExpo',
-	speed: 1250,
-	currentClass: 'active',
-	offset: 10,
+    easing: 'easeInOutExpo',
+    speed: 1250,
+    currentClass: 'active',
+    offset: 10,
 });
+
+
+function participantCtrl($scope) {
+    $scope.participantLastName = "";
+    $scope.participantFirstName = "";
+    $scope.participantFatherName = "";
+    $scope.participantPhoto = "";
+
+    $scope.myData = new Firebase("https://sizzling-torch-6523.firebaseio.com/");
+    $scope.saveParticipant = function() {
+        $scope.myData.push({participantLastName:$scope.participantLastName});
+        $scope.myData.push({participantFirstName:$scope.participantFirstName});
+        $scope.myData.push({participantFatherName:$scope.participantFatherName});
+        $scope.myData.push({participantPhoto:$scope.participantPhoto});
+    };
+};
+
