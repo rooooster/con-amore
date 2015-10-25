832e403b18c405b54a59d10e5d729b1924a33b55
Ihor Orlovskyi <ihor.droid@gmail.com>
2015-10-25 13:13:27 +0200

    about to-top

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,3 +125,27 @@ fotoramaDefaults = {
     autoplay: 3500
 }
 
+// toTop
+$(function() {
+ 
+$(window).scroll(function() {
+ 
+if($(this).scrollTop() != 0) {
+ 
+$('.to-top').fadeIn();
+ 
+} else {
+ 
+$('.to-top').fadeOut();
+ 
+}
+ 
+});
+ 
+$('.to-top').click(function() {
+ 
+$('body,html').animate({scrollTop:0},800);
+ 
+});
+ 
+});
