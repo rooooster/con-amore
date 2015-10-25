a45e905eb24ce4c31867a4eb419f9968d0476ff8
Ihor Orlovskyi <ihor.droid@gmail.com>
2015-10-25 04:57:13 +0200

    header styled

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,110 @@
 /**
+ * Cache
+ */
+var $content = $('.parallax-codepen .content')
+    , $blur    = $('.parallax-codepen .overlay')
+    , $normal    = $('.parallax-codepen .normal')
+    , wHeight  = $(window).height();
+
+$(window).on('resize', function(){
+    wHeight = $(window).height();
+});
+
+/**
+ * requestAnimationFrame Shim 
+ */
+window.requestAnimFrame = (function()
+{
+    return  window.requestAnimationFrame       ||
+                    window.webkitRequestAnimationFrame ||
+                    window.mozRequestAnimationFrame    ||
+                    function( callback ){
+                        window.setTimeout(callback, 1000 / 60);
+                    };
+})();
+
+/**
+ * Scroller
+ */
+function Scroller()
+{
+    this.latestKnownScrollY = 0;
+    this.ticking            = false;
+}
+
+Scroller.prototype = {
+    /**
+     * Initialize
+     */
+    init: function() {
+        window.addEventListener('scroll', this.onScroll.bind(this), false);
+    },
+
+    /**
+     * Capture Scroll
+     */
+    onScroll: function() {
+        this.latestKnownScrollY = window.scrollY;
+        this.requestTick();
+    },
+
+    /**
+     * Request a Tick
+     */
+    requestTick: function() {
+        if( !this.ticking ) {
+            window.requestAnimFrame(this.update.bind(this));
+        }
+        this.ticking = true;
+    },
+
+    /**
+     * Update.
+     */
+    update: function() {
+        var currentScrollY = this.latestKnownScrollY;
+        this.ticking       = false;
+        
+        /**
+         * Do The Dirty Work Here
+         */
+        var slowScroll = currentScrollY / 2,
+            mormalScroll =  currentScrollY / 2.75;
+        
+        $content.css({
+            'transform'         : 'translateY(-' + slowScroll + 'px)',
+            '-moz-transform'    : 'translateY(-' + slowScroll + 'px)',
+            '-webkit-transform' : 'translateY(-' + slowScroll + 'px)'
+        });
+        
+        $normal.css({
+            'transform'         : 'translateY(+' + mormalScroll + 'px)',
+            '-moz-transform'    : 'translateY(+' + mormalScroll + 'px)',
+            '-webkit-transform' : 'translateY(+' + mormalScroll + 'px)'
+        });
+    }
+};
+
+/**
+ * Attach!
+ */
+var scroller = new Scroller();
+scroller.init();
+
+
+
+
+
+
+
+
+
+
+/**
 * single-page-nav
 * https://github.com/ChrisWojcik/single-page-nav
 */
-$('#anhors').singlePageNav({
+$('#main').singlePageNav({
     easing: 'easeInOutExpo',
     speed: 1250,
     currentClass: 'active',
