c464fb8d145aff436f1ff83560c86eff6866e971
Ihor Orlovskyi <ihor.droid@gmail.com>
2016-01-30 04:16:46 +0200

    add lazy load

--- assets/js/main.js
+++ assets/js/main.js
@@ -215,7 +215,7 @@ $('.open-popup-link').magnificPopup({
 * https://github.com/tuupola/jquery_lazyload
 */
 $(function() {
-    $("img.js-lazy").lazyload({
+    $("img.lazy").lazyload({
       // threshold : 0,
       effect : "fadeIn",
       // placeholder : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
